f96c6b2f9d13fb452429f7aa72d8c7eea8ef5b90
helkuc <hkucharzyk@gmail.com>
2024-06-23 23:38:51 +0200

    poprawki

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import tkinter.messagebox as messagebox
 
 #połączanie z bazą danych
 def connectToDatabase():
-    config = {"user": "root", "password": "XXX", "host": "localhost", "database": "teatr"}
+    config = {"user": "root", "password": "XXXX", "host": "localhost", "database": "teatr"}
     global connect
     connect = mysql.connector.connect(**config)
     global cursor
